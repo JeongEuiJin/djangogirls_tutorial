1e3cfb36999c697ebd309e711e66e319cf77013f
jeong-eui-jin <hehar1020@gmail.com>
2017-11-01 00:22:39 +0900

    post_create 뷰생성

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,12 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 
 from blog.models import Post
 
+User = get_user_model()
+
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.order_by('-created_date')
     # posts = Post.objects.filter(published_date__lte=timezone.now())
     context = {
         'title': 'Django from djangogirls',
@@ -22,9 +25,21 @@ def post_detail(request, pk):
     }
     return render(request, 'blog/post_detail.html', context=context)
 
+
 def post_create(request):
+    if request.method == 'GET':
+        context = {
 
-    context = {
-
-    }
-    return render(request , 'blog/post_create.html', context=context)
+        }
+        return render(request, 'blog/post_create.html', context=context)
+    elif request.method == 'POST':
+        data = request.POST
+        title = data['title']
+        text = data['text']
+        user = User.objects.first()
+        post = Post.objects.create(
+            author=user,
+            title=title,
+            text=text
+        )
+        return redirect('post_detail', pk=post.pk)
